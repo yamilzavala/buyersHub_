ec6cb8d2c939f8c72037bc73fe3b8db7b6c595e9
Gustavo Cipriani <gustavocipriani@yahoo.com.ar>
2019-11-03 19:44:48 -0300

    Corregida config de CORS: Access-Control-Allow-Methods.

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST", "PUT", "GET", "DELETE", "OPTIONS");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
     next();
 });
 
