648ecbb49f2dc0d0a7f5b54dc0d35d3aa1d1b379
Gustavo Cipriani <gustavocipriani@yahoo.com.ar>
2019-11-03 21:32:24 -0300

    Corregida regresión imagen de detalle de producto y añadida la funcionalidad de suscripción / desuscripción.

--- frontend/src/app/pages/productos/detalle-producto/detalle-producto.component.ts
+++ frontend/src/app/pages/productos/detalle-producto/detalle-producto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductosService } from 'src/app/services/productos/productos.service';
 import { ActivatedRoute } from '@angular/router';
+import { ProductoModel } from '../models/producto.model';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -14,21 +15,21 @@ export class DetalleProductoComponent implements OnInit {
 
   constructor(public _productService: ProductosService, private _routeNav: ActivatedRoute) {
 
-    this._routeNav.params.subscribe( params => {
-              console.log(params);
-              const id_entero = parseInt(params['id'], 0);
-              this._productService.buscarProducto(id_entero)
-              // this._productService.buscarProducto('5dba42dba19fb11179353cf1')
-                      .subscribe( produto_res => {
-                            console.log(produto_res);
-                            this.producto = produto_res[0];
-                            this.validarImagen();
-                          }
-                      );
+    this._routeNav.params.subscribe(params => {
+      console.log(params);
+      const id_entero = parseInt(params['id'], 0);
+      this._productService.buscarProducto(id_entero)
+        // this._productService.buscarProducto('5dba42dba19fb11179353cf1')
+        .subscribe(produto_res => {
+          console.log(produto_res);
+          this.producto = produto_res[0];
+          this.validarImagen();
+        }
+        );
 
     });
 
-   }
+  }
 
   ngOnInit() {
   }
@@ -36,11 +37,28 @@ export class DetalleProductoComponent implements OnInit {
   validarImagen() {
     console.log(this.producto.imagen);
     if (this.producto.imagen === '') {
-      this.imagen = 'assets/images/not-image.png';
-    } else {
-      this.imagen = `assets/images/products/${this.producto.imagen}.jpg`;
-      console.log(this.imagen);
+      this.imagen = 'not-image.png';
     }
+  }
+
+  suscribir(producto: ProductoModel) {
+    // validacion de id de cliente, por propiedad "tieneCuentasAsociadas"
+    // Si tiene cuentas, muestra alert con mensaje de confirmacion
+    // Si no tiene cuentas asociadas, redirecciona a pantalle editar perfil, solapa cuentas
+    producto.estaSuscripto = true;
+    this.editarProducto(producto);
+  }
+
+  desuscribir(producto: ProductoModel) {
+    producto.estaSuscripto = false;
+    this.editarProducto(producto);
+  }
+
+  editarProducto(producto: ProductoModel) {
+    this._productService.editarProducto(producto)
+      .subscribe(res => {
+        console.log('Producto editado correctamente: ', res);
+      });
   }
 
 }
